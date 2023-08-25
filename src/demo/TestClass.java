18ebe1920f45b54b478375d9536d4684e24fb019
RohitSingh <thakurrohitdsingh@gmail.com>
2023-08-25 14:40:27 +0530

    ok

---
+++ src/demo/TestClass.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class TestClass {
+	
+	public static void main(String[] args) {
+		
+		Integer number = 10;
+		
+		if(number == 10) {
+			System.out.println("Ten");
+		}else {
+			System.out.println("Sothing Went Wrong");
+		}
+		
+	}
+
+}
